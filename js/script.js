82662f670269544d73b5b33534604913b9e9ed43
xeo3221 <95309495+xeo3221@users.noreply.github.com>
2024-01-03 23:40:10 +0100

    app-demo

---
+++ js/script.js
@@ -0,0 +1,57 @@
+document
+	.getElementById("generateBtn")
+	.addEventListener("click", generateRandomMeal)
+
+async function generateRandomMeal() {
+	try {
+		const response = await fetch(
+			"https://www.themealdb.com/api/json/v1/1/random.php"
+		)
+		const data = await response.json()
+
+		const meal = data.meals[0]
+		displayMeal(meal)
+	} catch (error) {
+		console.error("Error fetching random meal:", error)
+	}
+}
+
+function displayMeal(meal) {
+	const mealImage = document.getElementById("mealImage")
+	const mealName = document.getElementById("mealName")
+	const ingredientList = document.getElementById("ingredientList")
+
+	mealImage.src = meal.strMealThumb
+	mealName.textContent = meal.strMeal
+
+	ingredientList.innerHTML = ""
+
+	for (let i = 1; i <= 20; i++) {
+		const ingredient = meal[`strIngredient${i}`]
+		const measure = meal[`strMeasure${i}`]
+
+		if (ingredient && measure) {
+			const listItem = document.createElement("li")
+			listItem.textContent = `${measure} ${ingredient}`
+			ingredientList.appendChild(listItem)
+		}
+	}
+}
+
+const generateButton = document.getElementById("generateBtn")
+const ingredientsHeading = document.getElementById("ingredientsHeading")
+const container = document.querySelector(".container")
+
+let isGenerateButtonClicked = false
+
+generateButton.addEventListener("click", () => {
+	if (!isGenerateButtonClicked) {
+		isGenerateButtonClicked = true
+		ingredientsHeading.style.display = "block"
+	}
+})
+
+generateButton.addEventListener("click", () => {
+	container.style.width = "1000px"
+	container.style.height = "900px"
+})
